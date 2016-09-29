19cbd15461b0552a5f877c773ca94d63c61654ab
Martin Hinshelwood (nkdAgility) <martin@nkdagility.com>
2016-09-29 09:53:00 +0100

    building script for uploading process template

--- processtemplate-task/upload-process-template.ps1
+++ processtemplate-task/upload-process-template.ps1
@@ -1,12 +1,18 @@
 [CmdletBinding()]
 param(
     [string] $account,
-    [string] $accesstoken,
-    [string] $encoding,
-    [string] $failOnMissing, #keep for backward compatibility
-    [string] $tokenPrefix,
-    [string] $tokenSuffix,
-    [string] $writeBOM,
-    [string] $actionOnMissing,
-    [string] $keepToken
+    [string] $user,
+    [string] $token
 )
+
+
+$account = "http://nkdagility.visualstudio.com"
+$user = "martin@nkdagility.com"
+$token = "<testtoken>"
+# Base64-encodes the Personal Access Token (PAT) appropriately
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$token)))
+$headers = @{authorization=("Basic {0}" -f $base64AuthInfo)}
+$urllistprocess = "$($account)/_apis/process/processes?api-version=1.0"
+$stuff = Invoke-RestMethod -Uri $urllistprocess -Headers $headers -ContentType "application/json" -Method Get;
+
+$stuff
