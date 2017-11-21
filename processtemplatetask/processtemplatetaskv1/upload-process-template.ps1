ae01b0e6f2128a9f052ca9b658d563cdf758bd32
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2017-11-21 10:09:33 +0000

    Merged PR 122: Merge bug/5021-V1ContaminatedByV2Code to master
    Fix #5021 and rull back v1 to a previous release to fix old users...
    
    Related work items: #5021

--- processtemplatetask/processtemplatetaskv1/upload-process-template.ps1
+++ processtemplatetask/processtemplatetaskv1/upload-process-template.ps1
@@ -6,7 +6,7 @@ param(
 )
 $account = Get-VstsInput -Name account -Require
 $matchProcessTemplates = Get-VstsInput -Name matchProcessTemplates -Require
-$accesstoken = Get-VstsInput -Name processTemplateService -Require
+$accesstoken = Get-VstsInput -Name accesstoken -Require
 
 Write-VstsTaskVerbose "Account: $account" 
 Write-VstsTaskVerbose "matchProcessTemplates: $matchProcessTemplates" 
@@ -19,15 +19,18 @@ Write-VstsTaskVerbose "Building Base64 PAT Token"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$accesstoken)))
 $headers = @{authorization=("Basic {0}" -f $base64AuthInfo)}
 $headers.Add("X-TFS-FedAuthRedirect","Suppress")
-##########################################
-# Get a List of Templates
-##########################################
 $urllistprocess = "$($account)/_apis/process/processes?api-version=1.0"
 Write-VstsTaskVerbose "Calling $urllistprocess to get a list of current process templates."
 $templates = Invoke-RestMethod -Uri $urllistprocess -Headers $headers -ContentType "application/json" -Method Get;
 Try
 {
-    $jsontemplates = ConvertTo-Json $templates -ErrorAction Stop    
+    $jsontemplates = ConvertTo-Json $templates -ErrorAction Stop
+    Write-VstsTaskVerbose "Returned $($templates.count) processe templates on $account"
+    foreach ($pt in $templates.value)
+    {
+        Write-VstsTaskVerbose "Found $($pt.name): $($pt.url)"
+    }
+    
 }
 Catch
 {
@@ -36,17 +39,8 @@ Catch
     Write-VstsTaskError "Did not get a list of process templates back, twas not even JSON!"
     Write-VstsTaskError "Most common cause is that you did not authenticate correctly, check the Access Token."
     Write-VstsTaskError $ErrorMessage
-    exit 999
+    Break
 }
-# Write out the Tenplate list
-Write-VstsTaskVerbose "Returned $($templates.count) processe templates on $account"
-foreach ($pt in $templates.value)
-{
-    Write-Output "Found $($pt.name): $($pt.url)"
-}
-##########################################
-# Upload templates
-##########################################
 $urlPublishProcess = "$($account)/_apis/work/processAdmin/processes/import?ignoreWarnings=true&api-version=2.2-preview"
 $uploads = Get-ChildItem $matchProcessTemplates
 Write-Output "You have specified $($uploads.count) process templates to upload"
@@ -57,8 +51,14 @@ foreach ($file in $uploads)
     $result = Invoke-RestMethod -InFile $file -Uri $urlPublishProcess -Headers $headers -ContentType "application/zip" -Method Post;
     Try
     {
-        $uploadResult = ConvertTo-Json $result
-        Write-Output "$file uploaded, checking validatiopn"
+        $uploadResult = ConvertTo-Json $result -ErrorAction Stop
+        if ($result.validationResults.Count = 0)
+        {
+          Write-Output "$file sucessfully uploaded"
+        } else {
+           Write-VstsTaskError "Validation Failed for $file" 
+           Write-VstsTaskError $uploadResult
+        }
     }
     Catch
     {
@@ -66,16 +66,6 @@ foreach ($file in $uploads)
         $FailedItem = $_.Exception.ItemName
         Write-VstsTaskError "Did not get a result retunred from the upload, twas not even JSON!"
         Write-VstsTaskError $ErrorMessage
-        Write-VstsTaskError $result
-        exit 666
-    }
-    if ($result.validationResults.Count -eq 0)
-    {
-        Write-Output "$($file.Name) sucessfully validated"
-        exit 0
-    } else {
-        Write-VstsTaskError "Validation Failed for $($file.Name)"
-        Write-VstsTaskError $uploadResult
-        exit 1
+        Break
     }
 }
