0adbd22a060440042fb77db0c830da787ee304e0
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2017-11-27 17:55:14 +0000

    Added ability to unpack and edit GUID. This should allow the user to override the GUID in the UI for each process template uploaded.

--- processtemplatetask/processtemplatetaskv2/upload-process-template.ps1
+++ processtemplatetask/processtemplatetaskv2/upload-process-template.ps1
@@ -2,21 +2,25 @@
 param(
     [string] $account,
     [string] $accesstoken,
-    [string] $matchProcessTemplates
+    [string] $matchProcessTemplates,
+    [string] $overrideGuid
 )
-$account = Get-Input -Name account -Require
-$matchProcessTemplates = Get-Input -Name matchProcessTemplates -Require
-$endpoint = Get-Endpoint -Name processTemplateService -Require
+$account = Get-VstsInput -Name account -Require
+$matchProcessTemplates = Get-VstsInput -Name matchProcessTemplates -Require
+$endpointName = Get-VstsInput -Name processTemplateService -Require
+$endpoint = Get-VstsEndpoint -Name $endpointName -Require
+$OverrideProcessTemplateGUID = Get-VstsInput -Name overrideGuid
 
-$accesstoken = [string]$endpoint.auth.parameters.AccessToken
+$accesstoken = [string]$endpoint.Auth.Parameters.ApiToken
 
-Write-TaskVerbose "Account: $account" 
-Write-TaskVerbose "matchProcessTemplates: $matchProcessTemplates" 
-Write-TaskVerbose "accesstoken: $accesstoken" 
+Write-VstsTaskVerbose "Account: $account" 
+Write-VstsTaskVerbose "matchProcessTemplates: $matchProcessTemplates" 
+Write-VstsTaskVerbose "accesstoken: $accesstoken" 
+Write-VstsTaskVerbose "GUID: $OverrideProcessTemplateGUID" 
 
 
 #Write-Output "rootDirectory  " $rootDirectory
-Write-TaskVerbose "Building Base64 PAT Token"
+Write-VstsTaskVerbose "Building Base64 PAT Token"
 # Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$accesstoken)))
 $headers = @{authorization=("Basic {0}" -f $base64AuthInfo)}
@@ -25,7 +29,7 @@ $headers.Add("X-TFS-FedAuthRedirect","Suppress")
 # Get a List of Templates
 ##########################################
 $urllistprocess = "$($account)/_apis/process/processes?api-version=1.0"
-Write-TaskVerbose "Calling $urllistprocess to get a list of current process templates."
+Write-VstsTaskVerbose "Calling $urllistprocess to get a list of current process templates."
 $templates = Invoke-RestMethod -Uri $urllistprocess -Headers $headers -ContentType "application/json" -Method Get;
 Try
 {
@@ -35,49 +39,68 @@ Catch
 {
     $ErrorMessage = $_.Exception.Message
     $FailedItem = $_.Exception.ItemName
-    Write-TaskError "Did not get a list of process templates back, twas not even JSON!"
-    Write-TaskError "Most common cause is that you did not authenticate correctly, check the Access Token."
-    Write-TaskError $ErrorMessage
+    Write-VstsTaskError "Did not get a list of process templates back, twas not even JSON!"
+    Write-VstsTaskError "Most common cause is that you did not authenticate correctly, check the Access Token."
+    Write-VstsTaskError $ErrorMessage
     exit 999
 }
 # Write out the Tenplate list
-Write-TaskVerbose "Returned $($templates.count) processe templates on $account"
+Write-VstsTaskVerbose "Returned $($templates.count) processe templates on $account"
 foreach ($pt in $templates.value)
 {
     Write-Output "Found $($pt.name): $($pt.url)"
 }
 ##########################################
+# Fix Overrides
+##########################################
+$file = Get-ChildItem $matchProcessTemplates
+Write-VstsTaskVerbose $file
+if ($OverrideProcessTemplateGUID -ne $null )
+{
+    Write-VstsTaskVerbose "***RUNNNING OVERIDES****"
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $workFolder = [System.IO.Path]::Combine($file.Directory.FullName, "template")
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($file, $workFolder)
+    $processFile = "$workFolder\ProcessTemplate.xml"
+    $xml = [xml](get-content $processFile)
+    $guidXml = $xml.ProcessTemplate.metadata.version.Attributes.GetNamedItem("type")
+    $guid = $guidXml.'#text'
+     Write-VstsTaskVerbose "Current GUID is $guid and we are replacing it with $OverrideProcessTemplateGUID before upload"
+    $guidXml.'#text' = $OverrideProcessTemplateGUID
+    $xml.Save([string]$processFile)
+    [System.IO.File]::Move($file, "$file.old")
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($workFolder,$file)
+}
+
+##########################################
 # Upload templates
 ##########################################
-$urlPublishProcess = "$($account)/_apis/work/processAdmin/processes/import?ignoreWarnings=true&api-version=2.2-preview"
-$uploads = Get-ChildItem $matchProcessTemplates
-Write-Output "You have specified $($uploads.count) process templates to upload"
-Write-TaskVerbose $uploads
-foreach ($file in $uploads)
+#$urlPublishProcess = "$($account)/_apis/work/processAdmin/processes/import?ignoreWarnings=true&api-version=2.2-preview"
+$urlPublishProcess = "$($account)/_apis/work/processAdmin/processes/import?api-version=4.1-preview"
+Write-Output "Uploading $file" 
+$result = Invoke-RestMethod -InFile $file -Uri $urlPublishProcess -Headers $headers -ContentType "application/zip" -Method Post;
+Try
 {
-    Write-Output "Uploading $file" 
-    $result = Invoke-RestMethod -InFile $file -Uri $urlPublishProcess -Headers $headers -ContentType "application/zip" -Method Post;
-    Try
-    {
-        $uploadResult = ConvertTo-Json $result
-        Write-Output "$file uploaded, checking validatiopn"
-    }
-    Catch
-    {
-        $ErrorMessage = $_.Exception.Message
-        $FailedItem = $_.Exception.ItemName
-        Write-TaskError "Did not get a result retunred from the upload, twas not even JSON!"
-        Write-TaskError $ErrorMessage
-        Write-TaskError $result
-        exit 666
-    }
-    if ($result.validationResults.Count -eq 0)
-    {
-        Write-Output "$($file.Name) sucessfully validated"
-        exit 0
-    } else {
-        Write-TaskError "Validation Failed for $($file.Name)"
-        Write-TaskError $uploadResult
-        exit 1
-    }
+    $uploadResult = ConvertTo-Json $result
+    Write-Output "$file uploaded, checking validatiopn"
 }
+Catch
+{
+
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-VstsTaskError "Did not get a result retunred from the upload, twas not even JSON!"
+    Write-VstsTaskError $ErrorMessage
+    Write-VstsTaskError $result
+    exit 666
+}
+if ($result.validationResults.Count -eq 0)
+{
+    Write-Output "$($file.Name) sucessfully validated"
+    exit 0
+} else {
+    Write-VstsTaskError "Validation Failed for $($file.Name)"
+    Write-VstsTaskError $uploadResult
+    exit 1
+}
+
--- tests/debug-processtemplate-task.ps1
+++ tests/debug-processtemplate-task.ps1
@@ -1,18 +1,18 @@
 ﻿#Save-Module -Name VstsTaskSdk -Path ..\processtemplatetask\processtemplatetaskv2\ps_modules\
-Import-Module ..\processtemplatetask\processtemplatetaskv2\ps_modules\VstsTaskSdk\VstsTaskSdk.psm1
+Set-Location C:\Users\MartinHinshelwoodnkd\source\repos\vsts-processtemplate-task\tests
+Import-Module ..\processtemplatetask\processtemplatetaskv2\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1
 #Import-Module -Name VstsTaskSdk
 
 
 $erroractionpreference='stop'
 # Input 'MyInput':
-$env:INPUT_Account = "https://nkdagility.visualstudio.com"
-$env:INPUT_matchProcessTemplates = ""
+$env:INPUT_Account = "https://slb-swt-devbox.visualstudio.com"
+$env:INPUT_matchProcessTemplates = "c:\temp\pt\SLM-PT-VSTS-2017.7.0.zip"
+$env:INPUT_overrideGuid = "newGUID"
 
 $env:INPUT_processTemplateService = 'EP1'
 $env:ENDPOINT_URL_EP1 = 'https://myurl'
-$env:ENDPOINT_AUTH_EP1 = '{ "Parameters": { "AccessToken": "Sometokendata", }, "Scheme": "Token" }'
-$env:ENDPOINT_DATA_EP1 = '{ "Key1": "Value1", "Key2": "Value2" }'
-
+$env:ENDPOINT_AUTH_EP1 = '{ "Parameters": { "ApiToken": "5aofdqwf3aa7kyf7dvjptctjxqbtptjtavtxhf5airbhisuwekmq"}, "Scheme": "Token" }'
 
 Invoke-vstsTaskScript -scriptBlock { . ..\processtemplatetask\processtemplatetaskv2\upload-process-template.ps1 } -Verbose
 
