4c5663d84402d5c3999802e5365a351608ffb078
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2018-01-26 11:51:45 -0600

    Added v2 with fixed version for those currently using v2... Sync is being added to v3

---
+++ processtemplatetask/processtemplatetaskv2/importProcess.ps1
@@ -0,0 +1,125 @@
+[CmdletBinding()]
+param(
+    [string] $accountURL,
+    [string] $accesstoken,
+    [string] $processFile,
+    [string] $overrideGuid
+)
+#Main Inputs
+$processServiceName = Get-VstsInput -Name VstsXmlProcessService -Require
+$processFile = Get-VstsInput -Name processFile -Require
+$overrideProcessGuid = Get-VstsInput -Name overrideProcessGuid
+$overrideProcessName = Get-VstsInput -Name overrideProcessName
+# EndPoint 
+$processServiceEndpoint = Get-VstsEndpoint -Name $processServiceName -Require
+$accesstoken = [string]$processServiceEndpoint.Auth.Parameters.ApiToken
+$accountURL = [string]$processServiceEndpoint.Url
+
+get-childitem -path env:INPUT_*
+get-childitem -path env:ENDPOINT_*
+
+Write-VstsTaskVerbose "VSTS Account: $accountURL" 
+Write-VstsTaskVerbose "Process File: $processFile" 
+Write-VstsTaskVerbose "Acces Token: $accesstoken" 
+Write-VstsTaskVerbose "Process GUID: $overrideProcessGuid"
+Write-VstsTaskVerbose "Process Name: $overrideProcessName"
+
+
+#Write-Output "rootDirectory  " $rootDirectory
+Write-VstsTaskVerbose "Building Base64 PAT Token"
+# Base64-encodes the Personal Access Token (PAT) appropriately
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$accesstoken)))
+$headers = @{authorization=("Basic {0}" -f $base64AuthInfo)}
+$headers.Add("X-TFS-FedAuthRedirect","Suppress")
+##########################################
+# Get a List of Templates
+##########################################
+$urllistprocess = "$($accountURL)/_apis/process/processes?api-version=1.0"
+Write-VstsTaskVerbose "Calling $urllistprocess to get a list of current process templates."
+$templates = Invoke-RestMethod -Uri $urllistprocess -Headers $headers -ContentType "application/json" -Method Get;
+Try
+{
+    $jsontemplates = ConvertTo-Json $templates -ErrorAction Stop    
+}
+Catch
+{
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-VstsTaskError "Did not get a list of process templates back, twas not even JSON!"
+    Write-VstsTaskError "Most common cause is that you did not authenticate correctly, check the Access Token."
+    Write-VstsTaskError $ErrorMessage
+    exit 999
+}
+# Write out the Tenplate list
+Write-VstsTaskVerbose "Returned $($templates.count) processe templates on $accountURL"
+foreach ($pt in $templates.value)
+{
+    Write-Output "Found $($pt.name): $($pt.url)"
+}
+##########################################
+# Fix Overrides
+##########################################
+$file = Get-ChildItem $processFile
+Write-VstsTaskVerbose $file
+if ((($overrideProcessGuid -ne $null) -and ($overrideProcessGuid -ne "")) -or (($overrideProcessName -ne $null) -and ($overrideProcessName -ne "")))
+{
+    Write-VstsTaskVerbose "***RUNNNING OVERIDES****"
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $workFolder = [System.IO.Path]::Combine($file.Directory.FullName, "template")
+    if (Test-Path $workFolder)
+    {
+        [System.IO.Directory]::Delete($workFolder, $true)
+    }
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($file, $workFolder)
+    $processFile = "$workFolder\ProcessTemplate.xml"
+    $xml = [xml](get-content $processFile)
+    if (($overrideProcessGuid -ne $null) -and ($overrideProcessGuid -ne ""))
+    {
+        $guidXml = $xml.ProcessTemplate.metadata.version.Attributes.GetNamedItem("type")
+        $guid = $guidXml.'#text'
+         Write-VstsTaskVerbose "Current GUID is $guid and we are replacing it with $overrideProcessGuid before upload"
+        $guidXml.'#text' = $overrideProcessGuid
+    }
+    if (($overrideProcessName -ne $null) -and ($overrideProcessName -ne ""))
+    {
+        $nameXML = $xml.ProcessTemplate.metadata.name
+         Write-VstsTaskVerbose "Current Name of the Process is $nameXML and we are replacing it with $overrideProcessName before upload"
+        $xml.ProcessTemplate.metadata.name = $overrideProcessName
+    }
+    $xml.Save([string]$processFile)
+    [System.IO.File]::Delete($file)
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($workFolder,$file)
+}
+
+##########################################
+# Upload templates
+##########################################
+#$urlPublishProcess = "$($accountURL)/_apis/work/processAdmin/processes/import?ignoreWarnings=true&api-version=2.2-preview"
+$urlPublishProcess = "$($accountURL)/_apis/work/processAdmin/processes/import?api-version=4.0-preview.1"
+Write-Output "Uploading $file" 
+$result = Invoke-RestMethod -InFile $file -Uri $urlPublishProcess -Headers $headers -ContentType "application/zip" -Method Post;
+Try
+{
+    $uploadResult = ConvertTo-Json $result
+    Write-Output "$file uploaded, checking validatiopn"
+}
+Catch
+{
+
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+    Write-VstsTaskError "Did not get a result retunred from the upload, twas not even JSON!"
+    Write-VstsTaskError $ErrorMessage
+    Write-VstsTaskError $result
+    exit 666
+}
+if ($result.validationResults.Count -eq 0)
+{
+    Write-Output "$($file.Name) sucessfully validated"
+    exit 0
+} else {
+    Write-VstsTaskError "Validation Failed for $($file.Name)"
+    Write-VstsTaskError $uploadResult
+    exit 1
+}
+
