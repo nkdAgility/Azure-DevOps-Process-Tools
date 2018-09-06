df938546b9953bece68ab3ca34e1ee5c82fcc0f7
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2018-09-06 23:24:00 +0100

    Updated powershell to call process-migrate...

--- inheritedprocessmigration/vCurrent/processmigratestart-debug.ps1
+++ inheritedprocessmigration/vCurrent/processmigratestart-debug.ps1
@@ -1,5 +1,5 @@
 ﻿#Save-Module -Name VstsTaskSdk -Path ..\processtemplatetask\processtemplatetaskv2\ps_modules\
-#Set-Location C:\Users\MartinHinshelwoodnkd\source\repos\vsts-processtemplate-task\processtemplatetask\processtemplatetaskv2
+#Set-Location C:\Users\MartinHinshelwoodnkd\source\repos\vsts-processtemplate-task\inheritedprocessmigration\vCurrent
 Import-Module .\\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1
 #Import-Module -Name VstsTaskSdk
 #### Usefull bits ##############
@@ -9,20 +9,27 @@ Import-Module .\\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1
 ################################################
 ############### Test Data Setup ################ www.itprotoday.com/microsoft-azure/read-secret-azure-key-vault-using-powershell
 ################################################
-$TestDataFile = "c:\temp\pt\TestData.json"
+$TestDataFile = "c:\temp\inheritedprocessmigration\TestData.json"
 if (!(Test-Path $TestDataFile))
 {
    
-  $TestDataPS =  new-object psobject
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name processFile -Value "c:\temp\pt\SLM-PT-VSTS-2017.7.0.zip"
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name AccountURL -Value "https://xxx-xxx-devbox.visualstudio.com"
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name ApiToken -Value "mytoken"
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name waitForUpdate -Value $true
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name waitForInterval -Value 10
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name overrideProcessGuid -Value ""
-  Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name overrideProcessName -Value ""
-  $TestDataJson = ConvertTo-Json $TestDataPS
-  New-Item $TestDataFile -ItemType FILE -Value $TestDataJson -Force
+    $TestDataPS =  new-object psobject
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name command -Value "migrate"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name sourceAccountUrl -Value "https://nkdagility.visualstudio.com"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name sourceAccountToken -Value "mytoken"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name sourceProcessName -Value "nkdScrum"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name targetAccountUrl -Value "https://nkdagility-dev.visualstudio.com"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name targetAccountToken -Value "mytoken"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name targetProcessName -Value "nkdScrum"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name processFilename -Value "c:\temp\configuration.json"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name logLevel -Value "verbose"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name logFilename -Value "processMigrator.log"
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name overwritePicklist -Value $false
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name continueOnRuleImportFailure -Value $false
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name continueOnFieldDefaultValueFailure -Value $false
+    Add-Member -InputObject $TestDataPS -MemberType NoteProperty -Name skipImportFormContributions -Value $false
+    $TestDataJson = ConvertTo-Json $TestDataPS
+    New-Item $TestDataFile -ItemType FILE -Value $TestDataJson -Force
 }
 $TestDataJson = Get-Content $TestDataFile -Raw
 $TestData = ConvertFrom-Json $TestDataJson
@@ -31,16 +38,37 @@ $erroractionpreference='stop'
 ################################################
 ########### setup inputs #######################
 ################################################
-# Input 'MyInput':
-$env:INPUT_VstsXmlProcessService = 'EP1'
-$env:ENDPOINT_URL_EP1 = $TestData.AccountURL
-$env:ENDPOINT_AUTH_EP1 = "{ `"Parameters`": { `"ApiToken`": `"$($TestData.ApiToken)`"}, `"Scheme`": `"Token`" }"
-$env:ENDPOINT_DATA_EP1 = '{ "Key1": "Value1", "Key2": "Value2" }'
 
-$env:INPUT_processFile = $TestData.processFile
-$env:INPUT_waitForUpdate = $TestData.waitForUpdate
-$env:INPUT_waitForInterval = $TestData.waitForInterval
-$env:INPUT_overrideProcessGuid = $TestData.overrideProcessGuid
-$env:INPUT_overrideProcessName = $TestData.overrideProcessName
-Invoke-vstsTaskScript -scriptBlock { . .\importProcess.ps1 } -Verbose
+# Command
+$env:INPUT_command = $TestData.command
+# Source Account
+$env:INPUT_sourceAccount = 'EP1'
+$env:ENDPOINT_URL_EP1 = $TestData.sourceAccountUrl
+$env:ENDPOINT_AUTH_EP1 = "{ `"Parameters`": { `"ApiToken`": `"$($TestData.sourceAccountToken)`"}, `"Scheme`": `"Token`" }"
+$env:ENDPOINT_DATA_EP1 = '{ "Key1": "Value1", "Key2": "Value2" }'
+# sourceProcessName
+$env:INPUT_sourceProcessName = $TestData.sourceProcessName
+# Target Account
+$env:INPUT_targetAccount = 'EP2'
+$env:ENDPOINT_URL_EP2 = $TestData.targetAccountUrl
+$env:ENDPOINT_AUTH_EP2 = "{ `"Parameters`": { `"ApiToken`": `"$($TestData.targetAccountToken)`"}, `"Scheme`": `"Token`" }"
+$env:ENDPOINT_DATA_EP2 = '{ "Key1": "Value1", "Key2": "Value2" }'
+# targetProcessName
+$env:INPUT_targetProcessName = $TestData.targetProcessName
+# processFilename
+$env:INPUT_processFilename = $TestData.processFilename
+# logLevel
+$env:INPUT_logLevel = $TestData.logLevel
+# logFilename
+$env:INPUT_logFilename = $TestData.logFilename
+# overwritePicklist
+$env:INPUT_overwritePicklist = $TestData.overwritePicklist
+# continueOnRuleImportFailure
+$env:INPUT_continueOnRuleImportFailure = $TestData.continueOnRuleImportFailure
+# continueOnFieldDefaultValueFailure
+$env:INPUT_continueOnFieldDefaultValueFailure = $TestData.continueOnFieldDefaultValueFailure
+# skipImportFormContributions
+$env:INPUT_skipImportFormContributions = $TestData.skipImportFormContributions
+
+Invoke-vstsTaskScript -scriptBlock { . .\processmigratestart.ps1 } -Verbose
 
--- inheritedprocessmigration/vCurrent/processmigratestart.ps1
+++ inheritedprocessmigration/vCurrent/processmigratestart.ps1
@@ -1,4 +1,90 @@
-npm install process-migrator -g
-process-migrator
 
-Write-VstsTaskError ("Not yet implemented")
+
+# Command
+$command = Get-VstsInput -Name command -Require
+# sourceAccount
+$sourceAccountName = Get-VstsInput -Name sourceAccount -Require
+$sourceAccountEp = Get-VstsEndpoint -Name $sourceAccountName -Require
+$sourceAccountToken = [string]$sourceAccountEp.Auth.Parameters.ApiToken
+$sourceAccountUrl = [string]$sourceAccountEp.Url
+# sourceProcessName
+
+$sourceProcessName = Get-VstsInput -Name sourceProcessName -Require
+# TargetAccount
+$targetAccountName = Get-VstsInput -Name targetAccount -Require
+$targetAccountEp = Get-VstsEndpoint -Name $targetAccountName -Require
+$targetAccountToken = [string]$targetAccountEp.Auth.Parameters.ApiToken
+$targetAccountUrl = [string]$targetAccountEp.Url
+# targetProcessName
+$targetProcessName = Get-VstsInput -Name targetProcessName -Require
+# processFilename
+$processFilename = Get-VstsInput -Name processFilename -Require
+# logLevel
+$logLevel = Get-VstsInput -Name logLevel -Require
+# logFilename
+$logFilename = Get-VstsInput -Name logFilename -Require
+# overwritePicklist
+$overwritePicklist = Get-VstsInput -Name overwritePicklist -Require
+# continueOnRuleImportFailure
+$continueOnRuleImportFailure = Get-VstsInput -Name continueOnRuleImportFailure -Require
+# continueOnFieldDefaultValueFailure
+$continueOnFieldDefaultValueFailure = Get-VstsInput -Name continueOnFieldDefaultValueFailure -Require
+# skipImportFormContributions
+$skipImportFormContributions = Get-VstsInput -Name skipImportFormContributions -Require
+
+get-childitem -path env:INPUT_*
+get-childitem -path env:ENDPOINT_*
+
+Write-VstsTaskVerbose "command: $command" 
+Write-VstsTaskVerbose "sourceAccountUrl: $sourceAccountUrl" 
+Write-VstsTaskVerbose "targetProcessName: $targetProcessName" 
+Write-VstsTaskVerbose "targetAccountUrl: $targetAccountUrl" 
+Write-VstsTaskVerbose "targetProcessName: $targetProcessName" 
+
+###########################################################
+
+Get-Location
+
+$configFile = "configuration.json"
+if (!(Test-Path $configFile))
+{
+   
+    $configObject =  new-object psobject
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name sourceAccountUrl -Value $sourceAccountUrl
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name sourceAccountToken -Value $sourceAccountToken
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name sourceProcessName -Value $sourceProcessName
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name targetAccountUrl -Value $targetAccountUrl
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name targetAccountToken -Value $targetAccountToken
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name targetProcessName -Value $targetProcessName
+
+    $configOptionsObject =  new-object psobject
+
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name processFilename -Value $processFilename
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name logLevel -Value $logLevel
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name logFilename -Value $logFilename
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name overwritePicklist -Value [bool]$overwritePicklist
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name continueOnRuleImportFailure -Value [bool]$continueOnRuleImportFailure
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty -Name continueOnFieldDefaultValueFailure -Value [bool]$continueOnFieldDefaultValueFailure
+    Add-Member -InputObject $configOptionsObject -MemberType NoteProperty] -Name skipImportFormContributions -Value [bool]$skipImportFormContributions
+
+    Add-Member -InputObject $configObject -MemberType NoteProperty -Name options -Value $configOptionsObject
+
+    $configJson = ConvertTo-Json $configObject
+    New-Item $configFile -ItemType FILE -Value $configJson -Force
+}
+$configJson = Get-Content $configFile -Raw
+$ConfigData = ConvertFrom-Json $configJson
+
+$ConfigData
+
+######
+
+###########################################################
+
+npm install process-migrator -g
+
+Write-VstsTaskVerbose $command
+
+process-migrator --mode $command --config $configFile
+
+
